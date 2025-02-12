799291c57c53830a52fdf56decf77fc747f341e9
Stacy <stacylozovan@gmail.com>
2025-02-12 13:50:29 +0100

    changed the TrainCompartment

--- Week1/Exercise1/src/nl/saxion/oop/week1/TrainCompartment.java
+++ Week1/Exercise1/src/nl/saxion/oop/week1/TrainCompartment.java
@@ -4,4 +4,36 @@ public class TrainCompartment {
     public int numberOfSeats;
     public int compartmentClass;
     public int numberOfSeatsInUse;
+
+    public TrainCompartment(int numberOfSeats, int compartmentClass, int numberOfSeatsInUse) {
+        this.numberOfSeats = numberOfSeats;
+        this.compartmentClass = compartmentClass;
+        this.numberOfSeatsInUse = numberOfSeatsInUse;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public int getCompartmentClass() {
+        return compartmentClass;
+    }
+
+    public void setCompartmentClass(int compartmentClass) {
+        this.compartmentClass = compartmentClass;
+    }
+
+    public int getNumberOfSeatsInUse() {
+        return numberOfSeatsInUse;
+    }
+
+    public void setNumberOfSeatsInUse(int numberOfSeatsInUse) {
+        this.numberOfSeatsInUse = numberOfSeatsInUse;
+    }
 }
+
+
